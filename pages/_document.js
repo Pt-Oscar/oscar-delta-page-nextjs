c56efb3a5742b84dcfd3b39ff83df68fd6038e56
Miguel Leo <usuario@Mac-mini-de-Miguel-Leo-2.local>
2020-11-01 19:34:09 -0600

    add new font, and padding on tal

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class CustomDocument extends Document {
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
                     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
                     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-                </Head>
+                    <link href="https://fonts.googleapis.com/css2?family=Asap&display=swap" rel="stylesheet"/>                </Head>
                 <body>
                     <Main />
                 </body>
